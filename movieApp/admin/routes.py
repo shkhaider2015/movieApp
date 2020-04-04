a036d4609fe5a124c2416ccbdc5fa3a1cc3919d0
Haider <shkhaider2015@gmail.com>
2020-04-05 02:25:37 +0500

    admin ......

--- movieApp/admin/routes.py
+++ movieApp/admin/routes.py
@@ -1,15 +1,23 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, redirect, url_for
 from movieApp.admin.forms import LoginForm
 
 admin = Blueprint('admin', __name__)
 
 
 @admin.route('/admin')
-def admin():
+def my_admin():
     return render_template("admin.html", title="Admin Page")
 
 
 @admin.route('/admin/login', methods=['GET', 'POST'])
-def login():
+def admin_login():
+    email = "shkhaider2015@gmail.com"
+    password = "1234"
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == email and form.password.data == password:
+            redirect(url_for('admin.my_admin'))
+            flash("Login successfully !", 'success')
+        else:
+            flash("Check email and password", 'danger')
     return render_template("admin_login.html", title="Admin Page", form=form)
