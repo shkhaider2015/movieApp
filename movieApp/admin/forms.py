ec4fa11b7e5aa8b7f893705b7a06dc9f43014804
Haider <shkhaider2015@gmail.com>
2020-04-05 16:34:49 +0500

    admin ...

--- movieApp/admin/forms.py
+++ movieApp/admin/forms.py
@@ -1,9 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Email
+from wtforms import (StringField, SubmitField, PasswordField,
+                     FileField, SelectMultipleField)
+from wtforms.validators import DataRequired, Email, file_allowed
+from wtforms.fields.html5 import DateField
 
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Login as admin")
+
+
+class DataSubmitForm(FlaskForm):
+    movie_title = StringField("Title", validators=[DataRequired()])
+    movie_industry = StringField("Title", validators=[DataRequired()])
+    movie_genr = StringField("Title", validators=[DataRequired()])
+    movie_image = FileField("upload Picture", validators=[file_allowed(['jpg', 'png'])])
+    release_date = DateField("DatePicker", format="%Y-%m-%d")
+    cast = SelectMultipleField("Select Cast")
+    submit = SubmitField("Add")
--- movieApp/admin/routes.py
+++ movieApp/admin/routes.py
@@ -16,8 +16,13 @@ def admin_login():
     form = LoginForm()
     if form.validate_on_submit():
         if form.email.data == email and form.password.data == password:
-            redirect(url_for('admin.my_admin'))
+            redirect(url_for('admin.add_movies'))
             flash("Login successfully !", 'success')
         else:
             flash("Check email and password", 'danger')
     return render_template("admin_login.html", title="Admin Page", form=form)
+
+
+@admin.route('/admin/add_movie')
+def add_movies():
+    return render_template("add_movies.html", title="Admin Page")
