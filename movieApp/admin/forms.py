c562019856127d599cf39f74577563d9505bdbda
Haider <shkhaider2015@gmail.com>
2020-04-05 17:22:07 +0500

    admin ........

--- movieApp/admin/forms.py
+++ movieApp/admin/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import (StringField, SubmitField, PasswordField,
-                     FileField, SelectMultipleField)
-from wtforms.validators import DataRequired, Email, file_allowed
+                     SelectMultipleField)
+from wtforms.validators import DataRequired, Email
+from flask_wtf.file import FileField, file_allowed
 from wtforms.fields.html5 import DateField
 
 
@@ -13,9 +14,9 @@ class LoginForm(FlaskForm):
 
 class DataSubmitForm(FlaskForm):
     movie_title = StringField("Title", validators=[DataRequired()])
-    movie_industry = StringField("Title", validators=[DataRequired()])
-    movie_genr = StringField("Title", validators=[DataRequired()])
+    movie_industry = StringField("Industry", validators=[DataRequired()])
+    movie_genr = StringField("Genr", validators=[DataRequired()])
     movie_image = FileField("upload Picture", validators=[file_allowed(['jpg', 'png'])])
     release_date = DateField("DatePicker", format="%Y-%m-%d")
-    cast = SelectMultipleField("Select Cast")
+    # cast = SelectMultipleField("Select Cast", choices=[''])
     submit = SubmitField("Add")
--- movieApp/admin/routes.py
+++ movieApp/admin/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, flash, redirect, url_for
-from movieApp.admin.forms import LoginForm
+from movieApp.admin.forms import LoginForm, DataSubmitForm
 
 admin = Blueprint('admin', __name__)
 
@@ -9,6 +9,12 @@ def my_admin():
     return render_template("admin.html", title="Admin Page")
 
 
+@admin.route('/admin/add_movie')
+def add_data():
+    form = DataSubmitForm()
+    return render_template("add_movies.html", title="Admin Page", form=form)
+
+
 @admin.route('/admin/login', methods=['GET', 'POST'])
 def admin_login():
     email = "shkhaider2015@gmail.com"
@@ -16,13 +22,8 @@ def admin_login():
     form = LoginForm()
     if form.validate_on_submit():
         if form.email.data == email and form.password.data == password:
-            redirect(url_for('admin.add_movies'))
+            redirect(url_for('admin.add_data'))
             flash("Login successfully !", 'success')
         else:
             flash("Check email and password", 'danger')
     return render_template("admin_login.html", title="Admin Page", form=form)
-
-
-@admin.route('/admin/add_movie')
-def add_movies():
-    return render_template("add_movies.html", title="Admin Page")
