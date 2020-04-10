d39a84033cbb798f6440473f3dc932346aab386e
Haider <shkhaider2015@gmail.com>
2020-04-10 22:40:33 +0500

    upload form

--- movieApp/admin/routes.py
+++ movieApp/admin/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, flash, redirect, url_for, request
 from movieApp.admin.forms import LoginForm, DataSubmitForm
+from movieApp.models import Movie
 
 admin = Blueprint('admin', __name__)
 
@@ -9,10 +10,18 @@ def my_admin():
     return render_template("admin.html", title="Admin Page")
 
 
-@admin.route('/admin/add_movie')
+@admin.route('/admin/add_movie', methods=['GET', 'POST'])
 def add_data():
-    form = DataSubmitForm()
-    return render_template("my_custom_add_movies.html", title="Admin Page", form=form)
+    movie = Movie()
+    if request.method == 'POST':
+        movie.movie_title = request.form.get('movie_title')
+        movie.movie_industry = request.form.get('industry')
+        movie.release_date = request.form.get('released_date')
+        movie.movie_genr = request.form.getlist('GenrList')
+        movie.movie_image = request.form.get('movie_image')
+        movie.cast = request.form.get('cast')
+        print(f"Name : {movie.movie_title} Industry : {movie.movie_industry} Date : {movie.release_date} Genr : {movie.movie_genr} image : {movie.movie_image} cast : {movie.cast} ")
+    return render_template("my_custom_add_movies.html", title="Admin Page")
 
 
 @admin.route('/admin/login', methods=['GET', 'POST'])
--- movieApp/models.py
+++ movieApp/models.py
@@ -1,10 +1,10 @@
 from movieApp import db
 
 
-actors = db.Table('actors',
-                  db.Column('movie_id', db.Integer, db.ForeignKey('movie.movie_id'), primary_key=True),
-                  db.Column('actor_id', db.Ineteger, db.ForeignKey('actor.actor_id'), primary_key=True)
-                  )
+# actors = db.Table('actors',
+#                   db.Column('movie_id', db.Integer, db.ForeignKey('movie.movie_id'), primary_key=True),
+#                   db.Column('actor_id', db.Ineteger, db.ForeignKey('actor.actor_id'), primary_key=True)
+#                   )
 
 
 class Movie(db.Model):
@@ -13,19 +13,20 @@ class Movie(db.Model):
     movie_industry = db.Column(db.String(30), unique=False, nullable=False)
     movie_genr = db.Column(db.String(30), unique=False,
                            nullable=True, default="movies")
-    movie_image = db.Column(db.String(50), nullable=False,
+    movie_image = db.Column(db.String(1000), nullable=False,
                             default="movie_image.jpg")
-    release_date = db.Column(db.String(12))
-    cast = db.relationship('Actor', secondary=actors, lazy='subquery',
-                           backref=db.backref('movies', lazy=True)
-                           )
+    release_date = db.Column(db.String(100))
+    cast = db.Column(db.String, nullable=False)
+    # cast = db.relationship('Actor', secondary=actors, lazy='subquery',
+    #                        backref=db.backref('movies', lazy=True)
+    #                        )
 
 
-class Actor(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    actor_name = db.Column(db.String(30), unique=False, nullable=False)
-    actor_gender = db.Column(db.String(10), nullable=False,
-                             default="Not specify")
-    actor_image = db.Column(db.String(50), nullable=False,
-                            default="actor_image.jpg")
-    actor_age = db.Column(db.Integer)
+# class Actor(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     actor_name = db.Column(db.String(30), unique=False, nullable=False)
+#     actor_gender = db.Column(db.String(10), nullable=False,
+#                              default="Not specify")
+#     actor_image = db.Column(db.String(50), nullable=False,
+#                             default="actor_image.jpg")
+#     actor_age = db.Column(db.Integer)
